c9e7b019d779c47f12b8653d2fd06f21c23a96b1
bagriul <bagriul@gmail.com>
2023-10-17 14:13:53 +0300

    Update gcbotsmain.py

--- gcbotsmain.py
+++ gcbotsmain.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
 
     # Create separate processes for each script and start them concurrently
     processes.append(multiprocessing.Process(target=RentRegisterEmail.run))
-    processes.append(multiprocessing.Process(target=ObjectsNoTenant.run))
-    processes.append(multiprocessing.Process(target=NewRentTelegram2.run))
-    processes.append(multiprocessing.Process(target=ThreeiBot.run))
-    processes.append(multiprocessing.Process(target=UZ_Kyiv_Lviv_GC_Bot.run))
+    #processes.append(multiprocessing.Process(target=ObjectsNoTenant.run))
+    #processes.append(multiprocessing.Process(target=NewRentTelegram2.run))
+    #processes.append(multiprocessing.Process(target=ThreeiBot.run))
+    #processes.append(multiprocessing.Process(target=UZ_Kyiv_Lviv_GC_Bot.run))
     #processes.append(multiprocessing.Process(target=Protokoly.run))
 
     # Start all processes
